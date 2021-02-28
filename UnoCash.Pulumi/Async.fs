b23cbde14be4e5ec8388c71b56ef0c5d86548476
UnoSD <unosd.dev@gmail.com>
2021-02-28 22:55:46 +0000

    Async logic

--- UnoCash.Pulumi/Async.fs
+++ UnoCash.Pulumi/Async.fs
@@ -2,3 +2,12 @@ module Async
 
 let map f async' =
     async.Bind(async', f >> async.Return)
+
+let zip (x : Async<'a>) (y : Async<'b>) : Async<'a * 'b> =
+  [| map box x; map box y |] |>
+  Async.Parallel |>
+  map (fun xy -> unbox xy.[0], unbox xy.[1])
+  
+
+type AsyncBuilder with
+   member _.MergeSources (x, y) = zip x y
