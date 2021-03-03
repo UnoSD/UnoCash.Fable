16a2b7b248e2df42503cd51abaacfd9d5cb74f9a
UnoSD <unosd.dev@gmail.com>
2021-03-03 20:38:46 +0000

    Query string parser

--- UnoCash.Api/GetExpenses.fs
+++ UnoCash.Api/GetExpenses.fs
@@ -1,63 +1,74 @@
-namespace UnoCash.Api
+module UnoCash.Api.GetExpenses
 
 open System
 open UnoCash.Core
 open Microsoft.Azure.WebJobs
+open UnoCash.Api.HttpRequest
 open Microsoft.AspNetCore.Mvc
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.Logging
 open Microsoft.Azure.WebJobs.Extensions.Http
 
-module GetExpenses =
-    [<FunctionName("GetExpenses")>]
-    let run ([<HttpTrigger(AuthorizationLevel.Function, "get")>]req: HttpRequest) (log: ILogger) =
-        async {
-            log.LogInformation("Get expenses called")
-            
-            let accountResult =
-                match req.Query |> HttpRequest.tryGetValues "account" with
-                | Some [| "" |]    -> Error "Missing account name"
-                | Some [| value |] -> Ok value
-                | Some _           -> Error "Multiple accounts not supported"
-                | _                -> Error "Missing account name"
+let private parseGuid (value : string) =
+    match Guid.TryParse(value) with
+    | Value x -> Some x |> Ok
+    | _       -> Error "Invalid expense guid"
 
-            log.LogInformation("Get expenses for account: {account}", accountResult)
+let private ignoreArg y _ =
+    y
+
+[<FunctionName("GetExpenses")>]
+let run ([<HttpTrigger(AuthorizationLevel.Function, "get")>]req: HttpRequest) (log: ILogger) =
+    async {
+        log.LogInformation("Get expenses called")
+        
+        let accountResult =
+            {
+                Key      = "account"
+                Value    = Ok
+                Empty    = Error "Missing account name"
+                Missing  = Error "Missing account name"
+                Multiple = Error "Multiple accounts not supported" |> ignoreArg
+            } |> getQueryStringResult req.Query
+        
+        log.LogInformation("Get expenses for account: {account}", accountResult)
+            
+        let upnResult =
+            match req.Cookies.TryGetValue "jwtToken" with
+            | Value t -> JwtToken.getClaim "upn" t |>
+                         Option.map Ok |>
+                         Option.defaultValue (Error "Missing upn claim")                      
+            | _       -> Error "Missing jwtToken cookie"
+        
+        log.LogInformation("Get expenses for upn: {upn}", upnResult)
+        
+        let guidResult =
+            {
+                Key      = "id"
+                Value    = parseGuid
+                Empty    = Ok None
+                Missing  = Ok None
+                Multiple = Error "Multiple ids not supported" |> ignoreArg
+            } |> getQueryStringResult req.Query
+
+        log.LogInformation("Get expense for id: {guid}", guidResult)
+
+        let result =
+            result {
+                let! account = accountResult
+                and! upn = upnResult
+                and! guidResult = guidResult 
                 
-            let upnResult =
-                match req.Cookies.TryGetValue "jwtToken" with
-                | Value t -> JwtToken.getClaim "upn" t |>
-                             Option.map Ok |>
-                             Option.defaultValue (Error "Missing upn claim")                      
-                | _       -> Error "Missing jwtToken cookie"
-            
-            log.LogInformation("Get expenses for upn: {upn}", upnResult)
-            
-            let guidResult =
-                match req.Query |> HttpRequest.tryGetValues "id" with
-                | Some [| value |] -> match Guid.TryParse(value) with
-                                      | Value x -> Some x |> Ok
-                                      | _       -> Error "Invalid expense guid"
-                | Some _           -> Error "Multiple ids not supported"
-                | _                -> Ok None
-
-            log.LogInformation("Get expense for id: {guid}", guidResult)
-
-            let result =
-                result {
-                    let! account = accountResult
-                    and! upn = upnResult
-                    and! guidResult = guidResult 
-                    
-                    return match guidResult with
-                           | Some guid -> ExpenseReader.GetAsync(account, upn, guid) |> Async.AwaitTask
-                           | None      -> ExpenseReader.GetAllAsync(account, upn)    |> Async.AwaitTask
-                }
-            
-            return! match result with
-                    | Ok expensesAsync -> async {
-                                              let! expenses = expensesAsync
-                                              return OkObjectResult expenses :> IActionResult
-                                          } 
-                    | Error errors     -> async { return BadRequestObjectResult errors :> IActionResult }
-        } |>
-        Async.StartAsTask
+                return match guidResult with
+                       | Some guid -> ExpenseReader.GetAsync(account, upn, guid) |> Async.AwaitTask
+                       | None      -> ExpenseReader.GetAllAsync(account, upn)    |> Async.AwaitTask
+            }
+        
+        return! match result with
+                | Ok expensesAsync -> async {
+                                          let! expenses = expensesAsync
+                                          return OkObjectResult expenses :> IActionResult
+                                      } 
+                | Error errors     -> async { return BadRequestObjectResult errors :> IActionResult }
+    } |>
+    Async.StartAsTask
--- UnoCash.Api/HttpRequest.fs
+++ UnoCash.Api/HttpRequest.fs
@@ -12,3 +12,19 @@ let tryGetSingleValue key (query : IQueryCollection) =
     Option.bind (function
                  | [| value |] -> Some value
                  | _           -> None)
+    
+type QueryStringValidationSettings<'a, 'b> =
+    {
+        Key: string
+        Value: 'a -> 'b
+        Empty: 'b
+        Multiple: 'a array -> 'b
+        Missing: 'b
+    }
+
+let getQueryStringResult query config =
+    match query |> tryGetValues config.Key with
+    | Some [| "" |]    -> config.Empty
+    | Some [| value |] -> config.Value value
+    | Some values      -> config.Multiple values
+    | _                -> config.Missing
