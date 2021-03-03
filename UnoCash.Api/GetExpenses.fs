8e583398973f7b89b503e3f04169fbaa0b1f2998
UnoSD <unosd.dev@gmail.com>
2021-03-03 22:42:20 +0000

    Refactor GetExpenses

--- UnoCash.Api/GetExpenses.fs
+++ UnoCash.Api/GetExpenses.fs
@@ -5,54 +5,43 @@ open Microsoft.Azure.WebJobs
 open UnoCash.Api.HttpRequest
 open Microsoft.AspNetCore.Mvc
 open Microsoft.AspNetCore.Http
-open Microsoft.Extensions.Logging
 open Microsoft.Azure.WebJobs.Extensions.Http
 
 [<FunctionName("GetExpenses")>]
-let run ([<HttpTrigger(AuthorizationLevel.Function, "get")>]req: HttpRequest) (log: ILogger) =
+let run ([<HttpTrigger(AuthorizationLevel.Function, "get")>]req: HttpRequest) =
     async {
-        log.LogInformation("Get expenses called")
-        
-        let upnResult =
-            JwtToken.tryGetUpn req.Cookies
-        
-        log.LogInformation("Get expenses for upn: {upn}", upnResult)
-        
-        let accountResult =
-            {
-                Key      = "account"
-                Value    = Ok
-                Empty    = Error "Missing account name"
-                Missing  = Error "Missing account name"
-                Multiple = Error "Multiple accounts not supported" |> ignoreSnd
-            } |> getQueryStringResult req.Query
-        
-        log.LogInformation("Get expenses for account: {account}", accountResult)
-        
-        let guidResult =
-            {
-                Key      = "id"
-                Value    = tryParseGuid
-                Empty    = Error "Empty id value"
-                Missing  = Ok None
-                Multiple = Error "Multiple ids not supported" |> ignoreSnd
-            } |> getQueryStringResult req.Query
-
-        log.LogInformation("Get expense for id: {guid}", guidResult)
-
-        let result =
+        let response =
             result {
-                let! account = accountResult
-                and! upn = upnResult
-                and! guidResult = guidResult 
+                let! upn = 
+                    JwtToken.tryGetUpn req.Cookies
+                and! account =
+                    {
+                        Key      = "account"
+                        Value    = Ok
+                        Empty    = Error "Missing account name"
+                        Missing  = Error "Missing account name"
+                        Multiple = Error "Multiple accounts not supported" |> ignoreSnd
+                    } |> getQueryStringResult req.Query
+                and! guidOption =
+                    {
+                        Key      = "id"
+                        Value    = tryParseGuid
+                        Empty    = Error "Empty id value"
+                        Missing  = Ok None
+                        Multiple = Error "Multiple ids not supported" |> ignoreSnd
+                    } |> getQueryStringResult req.Query 
                 
-                return match guidResult with
-                       | Some guid -> ExpenseReader.GetAsync(account, upn, guid) |> Async.AwaitTask
-                       | None      -> ExpenseReader.GetAllAsync(account, upn)    |> Async.AwaitTask
-            }
+                let task =
+                    match guidOption with
+                    | Some guid -> ExpenseReader.GetAsync(account, upn, guid) |> Async.AwaitTask
+                    | None      -> ExpenseReader.GetAllAsync(account, upn)    |> Async.AwaitTask
+                
+                return task
+            } |>
+            function
+            | Ok expensesAsync -> expensesAsync |> Async.map (OkObjectResult >> unbox)
+            | Error errors     -> errors |> BadRequestObjectResult :> IActionResult |> async.Return
         
-        return! match result with
-                | Ok expensesAsync -> expensesAsync |> Async.map (OkObjectResult >> unbox)
-                | Error errors     -> errors |> BadRequestObjectResult :> IActionResult |> async.Return
+        return! response
     } |>
     Async.StartAsTask
