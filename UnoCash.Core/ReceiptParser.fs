691b41057f348e9980f17856b08f48a5ae9a616a
UnoSD <unosd.dev@gmail.com>
2021-03-08 09:51:43 +0000

    First pass ReceiptParser refactoring

--- UnoCash.Core/ReceiptParser.fs
+++ UnoCash.Core/ReceiptParser.fs
@@ -2,7 +2,9 @@ module UnoCash.Core.ReceiptParser
 
 open System
 open System.Collections.Generic
+open System.IO
 open System.Text.Json
+open System.Threading.Tasks
 open Azure
 open Azure.AI.FormRecognizer
 open UnoCash.Dto
@@ -21,6 +23,73 @@ let getOrDefault key (dict : IReadOnlyDictionary<string, UnoCashFormField>) (def
     | Value v -> changeTypeOrDefault v default'
     | _       -> default'
 
+let getReceiptDataFromCache (streamTask : Task<Stream>) =
+    async {
+        use! stream = 
+            streamTask
+        
+        printfn "Found receipt analysis in cache"
+        
+        let! fields =
+            JsonSerializer.DeserializeAsync<Dictionary<string, UnoCashFormField>>(stream)
+        
+        return {
+            Payee  = getOrDefault "MerchantName" fields ""
+            Date   = getOrDefault "TransactionDate" fields DateTime.Today
+            Method = "Cash"
+            Amount = getOrDefault "Total" fields 0m
+        }
+    }
+
+let analyseReceipt (container : CloudBlobContainer) blobName =
+    async {
+        printfn "Analysing receipt"
+        
+        let blob = container.GetBlobReference(blobName)
+        
+        let endpoint =
+            Configuration.tryGetSetting "FormRecognizerEndpoint" |> Option.defaultValue null
+
+        let formRecognizerKey =
+            Configuration.tryGetSetting "FormRecognizerKey" |> Option.defaultValue null
+        
+        let sas = blob.GetSharedAccessSignature(SharedAccessBlobPolicy
+                            (
+                                Permissions            = SharedAccessBlobPermissions.Read,
+                                SharedAccessExpiryTime = DateTimeOffset.Now.AddMinutes(5.)
+                            ))
+
+        let blobUrl = blob.Uri.ToString() + sas
+
+        let credential = AzureKeyCredential(formRecognizerKey)
+        let formRecognizerClient = FormRecognizerClient(Uri(endpoint), credential)
+
+        let! request =
+            formRecognizerClient.StartRecognizeReceiptsFromUriAsync(Uri(blobUrl))
+
+        let! response =
+            request.WaitForCompletionAsync()
+
+        let recognizedForm = response.Value |> Seq.exactlyOne
+
+        let json = JsonSerializer.Serialize(recognizedForm.Fields,
+                                            JsonSerializerOptions (WriteIndented = true));
+
+        do! container.GetBlockBlobReference(blobName + ".json")
+                     .UploadTextAsync(json)
+
+        printfn "%s" json
+
+        let fields = JsonSerializer.Deserialize<Dictionary<string, UnoCashFormField>>(json)
+        
+        return {
+           Payee  = getOrDefault "MerchantName" fields ""
+           Date   = getOrDefault "TransactionDate" fields DateTime.Today
+           Method = "Cash"
+           Amount = getOrDefault "Total" fields 0m
+       }
+    }
+
 let parseAsync blobName =
     async {    
         let container =
@@ -31,65 +100,11 @@ let parseAsync blobName =
             
         let! exists =   
             cachedResultsBlob.ExistsAsync()
+          
+        let receipt =
+            match exists with
+            | true  -> cachedResultsBlob.OpenReadAsync() |> getReceiptDataFromCache
+            | false -> analyseReceipt container blobName
             
-        if exists then
-            use! stream = cachedResultsBlob.OpenReadAsync()
-            
-            printfn "Found receipt analysis in cache"
-            
-            let! fields =
-                JsonSerializer.DeserializeAsync<Dictionary<string, UnoCashFormField>>(stream)
-            
-            return {
-                Payee  = getOrDefault "MerchantName" fields ""
-                Date   = getOrDefault "TransactionDate" fields DateTime.Today
-                Method = "Cash"
-                Amount = getOrDefault "Total" fields 0m
-            }
-        else
-            printfn "Analysing receipt"
-            
-            let blob = container.GetBlobReference(blobName)
-            
-            let endpoint =
-                Configuration.tryGetSetting "FormRecognizerEndpoint" |> Option.defaultValue null
-
-            let formRecognizerKey =
-                Configuration.tryGetSetting "FormRecognizerKey" |> Option.defaultValue null
-            
-            let sas = blob.GetSharedAccessSignature(SharedAccessBlobPolicy
-                                (
-                                    Permissions            = SharedAccessBlobPermissions.Read,
-                                    SharedAccessExpiryTime = DateTimeOffset.Now.AddMinutes(5.)
-                                ))
-
-            let blobUrl = blob.Uri.ToString() + sas
-
-            let credential = AzureKeyCredential(formRecognizerKey)
-            let formRecognizerClient = FormRecognizerClient(Uri(endpoint), credential)
-
-            let! request =
-                formRecognizerClient.StartRecognizeReceiptsFromUriAsync(Uri(blobUrl))
-
-            let! response =
-                request.WaitForCompletionAsync()
-
-            let recognizedForm = response.Value |> Seq.exactlyOne
-
-            let json = JsonSerializer.Serialize(recognizedForm.Fields,
-                                                JsonSerializerOptions (WriteIndented = true));
-
-            do! container.GetBlockBlobReference(blobName + ".json")
-                         .UploadTextAsync(json)
-
-            printfn "%s" json
-
-            let fields = JsonSerializer.Deserialize<Dictionary<string, UnoCashFormField>>(json)
-            
-            return {
-               Payee  = getOrDefault "MerchantName" fields ""
-               Date   = getOrDefault "TransactionDate" fields DateTime.Today
-               Method = "Cash"
-               Amount = getOrDefault "Total" fields 0m
-           }
+        return! receipt
     }
