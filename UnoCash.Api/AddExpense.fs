1186a8954bcb9064d813b3e26fb0e442495f9046
UnoSD <1582987+UnoSD@users.noreply.github.com>
2022-08-16 23:00:04 +0200

    Catch and log add expense exception

--- UnoCash.Api/AddExpense.fs
+++ UnoCash.Api/AddExpense.fs
@@ -1,11 +1,12 @@
 module UnoCash.Api.AddExpenses
 
+open Microsoft.Extensions.Logging
 open UnoCash.Core
 open Microsoft.Azure.WebJobs
 open Microsoft.AspNetCore.Http
 open Microsoft.Azure.WebJobs.Extensions.Http
 
 [<FunctionName("AddExpense")>]
-let run ([<HttpTrigger(AuthorizationLevel.Anonymous, "post")>]req: HttpRequest) =
+let run ([<HttpTrigger(AuthorizationLevel.Anonymous, "post")>]req: HttpRequest, logger : ILogger) =
     // Validate Currency
-    AddDto.add req ExpenseWriter.writeAsync
+    AddDto.add req (ExpenseWriter.writeAsync (fun exn msg args -> logger.LogError(exn, msg, args)))
--- UnoCash.Core/ExpenseWriter.fs
+++ UnoCash.Core/ExpenseWriter.fs
@@ -1,13 +1,54 @@
 module UnoCash.Core.ExpenseWriter
 
+open Microsoft.Azure.Cosmos.Table
 open UnoCash.Core.Table
 open UnoCash.Dto
 open System
 open FSharp.Azure.Storage.Table
 open UnoCash.Core.ExpenseTable
 
-let writeAsync upn (expense : Expense) =
-    writeAsync upn expense getPartitionKey getRowKey
+// Use CLI_DEBUG=1 as env var to get the exception logged in the func console
+
+let writeAsync (log : exn -> string -> obj[] -> unit) upn (expense : Expense) =
+    async {
+        let! result = 
+            writeAsync upn expense getPartitionKey getRowKey
+            |> Async.Catch
+            
+        let errorOperationResult =
+            {
+                HttpStatusCode = 500
+                Etag = String.Empty
+            }
+        
+        let formatExtendedErrorInformation (sexn : StorageException) =
+            sexn.RequestInformation.ExtendedErrorInformation
+            |> Option.ofObj
+            |> Option.map (fun x -> x.ToString())
+            |> Option.defaultValue "null"
+        
+        let logStorageException (sexn : StorageException) =
+            log sexn
+                ("Message: {Message}" +
+                 "StatusCode: {HttpStatusCode}" +
+                 "ExtendedInfo: {ExtendedErrorInformation}" +
+                 "Code: {ErrorCode}")
+                [| sexn.Message
+                   sexn.RequestInformation.HttpStatusCode                   
+                   formatExtendedErrorInformation sexn                   
+                   sexn.RequestInformation.ErrorCode |]
+        
+        let logAggregateException (aexn : AggregateException) =
+            match aexn.Flatten().InnerExceptions |> List.ofSeq with
+            | [ :? StorageException as sexn ] -> logStorageException sexn
+            | _                               -> log aexn "Error creating expense {Expense}" [| expense |]
+        
+        return match result with
+               | Choice1Of2 operationResult                 -> operationResult
+               | Choice2Of2 (:? AggregateException as aexn) -> logAggregateException aexn
+                                                               errorOperationResult
+               | Choice2Of2 _                               -> errorOperationResult
+    }
     
 let deleteAsync upn account (id : Guid) =
     {
